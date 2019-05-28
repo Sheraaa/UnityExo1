dbb876950222d310091c7ccf857ed2d5f7b0fd6f
Sherazade <chehrazad.ouazzani@hotmail.fr>
2019-05-28 16:18:46 +0200

    Jeux de Memory

---
+++ Memory/Assets/Scripts/MemoryCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCube : MonoBehaviour {
+    public Color baseColor;
+    private Renderer render;
+    bool clicked = false;
+
+	// Use this for initialization
+	void Start () {
+        render = GetComponent<Renderer>();
+        render.material.color = baseColor;
+    }
+
+    private void OnMouseDown() {
+        if (render.material.color == baseColor) {
+            render.material.color = Color.red;
+        } else {
+            render.material.color = baseColor;
+        }
+    }
+
+
+}
